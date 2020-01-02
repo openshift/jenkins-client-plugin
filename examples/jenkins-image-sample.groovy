4b522612553bd47c12b654a53220516686259d03
Akram Ben Aissi <akram.benaissi@gmail.com>
2020-01-02 18:46:24 +0100

    Verify that template does not exist before creation

--- examples/jenkins-image-sample.groovy
+++ examples/jenkins-image-sample.groovy
@@ -42,11 +42,14 @@ void actualTest() {*/
             openshift.withCluster() {
                 // Test openshift.patch and selector.patch
                 openshift.withProject() {
-                    openshift.create("https://raw.githubusercontent.com/openshift/nodejs-ex/master/openshift/templates/nodejs.json")
-                    openshift.newApp("nodejs-example")
-                    openshift.patch("dc/nodejs-example", '\'{"spec":{"strategy":{"type":"Recreate"}}}\'')
-                    def mySelector = openshift.selector("bc/nodejs-example")
-                    mySelector.patch('\'{"spec":{"source":{"git":{"ref": "development"}}}}\'')
+                  def templateSelector = openshift.selector( "template", "nodejs-example")
+                  if (!templateSelector.exists() ) {
+                      openshift.create("https://raw.githubusercontent.com/openshift/nodejs-ex/master/openshift/templates/nodejs.json")
+                  } 
+                  openshift.newApp("nodejs-example")
+                  openshift.patch("dc/nodejs-example", '\'{"spec":{"strategy":{"type":"Recreate"}}}\'')
+                  def mySelector = openshift.selector("bc/nodejs-example")
+                  mySelector.patch('\'{"spec":{"source":{"git":{"ref": "development"}}}}\'')
                 }
                 // Select the default project
                 openshift.withProject() {
