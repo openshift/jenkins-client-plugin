2c888eb3f93dc9fef50246b7bfdaad6ff39793b9
gabemontero <gmontero@redhat.com>
2022-04-23 10:54:13 -0400

    tag CI jenkins-agent-base image into test cluster for testing

--- hack/tag-ci-image.sh
+++ hack/tag-ci-image.sh
@@ -14,3 +14,17 @@ else
     echo "New contents of the jenkins imagestream in the openshift namespace"
     oc describe is jenkins -n openshift
 fi
+echo "Jenkins Agent Base image from CI pipeline:jenkins-agent-base: ${JENKINS_AGENT_BASE_IMAGE}"
+if [[ -z ${JENKINS_AGENT_BASE_IMAGE} ]]; then
+    echo "No jenkins agent base image env var found, not overriding jenkins-agent-base imagestream for e2e test of jenkins-client-plugin."
+else
+    echo "Tagging the CI generated Jenkins Agent Base image ${JENKINS_AGENT_BASE_IMAGE} from pipeline:jenkins-agent-base into the test cluster's jenkins-agent-base imagestream in the openshift namespace"
+    echo "Current contents of the jenkins-agent-base imagestream in the openshift namespace"
+    oc describe is jenkins-agent-base -n openshift
+    echo "Tagging ${JENKINS_AGENT_BASE_IMAGE} into the jenkins-agent-base imagestream in the openshift namespace"
+    oc tag --source=docker ${JENKINS_AGENT_BASE_IMAGE} openshift/jenkins-agent-base:latest
+    # give some time for the image import to finish; watching from the CLI is non-trivial
+    sleep 30
+    echo "New contents of the jenkins imagestream in the openshift namespace"
+    oc describe is jenkins-agent-base -n openshift
+fi
